f1ca98edaf30ffec2087c422733bcd704678ec68
David Schuld <davidschuld@gmail.com>
2016-07-13 23:05:28 +0200

    - Changed flexible header impl - Adapted css colors to monochrome blueish style

--- js/flexible-header.js
+++ js/flexible-header.js
@@ -4,71 +4,111 @@ var lastScrollTop = 0;
 var delta = 75;
 var deltaDown = 400;
 var navbarHeight = $('.site-header').outerHeight();
+var offset = 0;
+var hidden = false;
 
 
-$(window).scroll(function(event){
-didScroll = true;
+$(window).scroll(function (event) {
+    didScroll = true;
 
 });
 
-setInterval(function() {
-	if (didScroll) {
-		hasScrolled();
-		didScroll = false;
-	}
+setInterval(function () {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
 }, 250);
 
+
+/*
+ * Bei click auf home oder post menu:
+ * - falls header up, main auch up
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
 function showMap() {
-	$('.site-header').removeClass('nav-up').addClass('nav-down');
-	$('.show-map').removeClass('show-map').addClass('hide-map');
-	$('.hide-map a').html('Hide Map');
-	//$('.site-main').removeClass('main-up').addClass('main-down');
-	
+    if (!hidden) {
+        return;
+    }
+
+    hidden = false;
+    $('.site-header').removeClass('nav-up').addClass('nav-down');
+    $('.show-map').removeClass('show-map').addClass('hide-map');
+    $('.hide-map a').html('Hide Map');
+    $('.site-main').css({
+        'top': 0
+    });
+
 }
+
 
 function hideMap() {
-	$('.site-header').removeClass('nav-down').addClass('nav-up');
-	$('.hide-map').removeClass('hide-map').addClass('show-map');
-	$('.show-map a').html('Show Map');
-	//$('.site-main').removeClass('main-down').addClass('main-up');
-	
+    if (hidden) {
+        return;
+    }
+
+    hidden = true;
+
+    $('.site-header').removeClass('nav-down').addClass('nav-up');
+
+    offset = 500 - $(this).scrollTop();
+
+    if (offset > 0) {
+        $('.site-main').css({
+            'top': 0 - offset
+        });
+
+    }
+
+
+    $('.hide-map').removeClass('hide-map').addClass('show-map');
+    $('.show-map a').html('Show Map');
+
 }
 
 
 
-(function($) {
+(function ($) {
 
-	$(document).on( 'click', '.hide-map a', function( event ) {
-		event.preventDefault();
-		hideMap();
-	});
+    $(document).on('click', '.hide-map a', function (event) {
+        event.preventDefault();
+        hideMap();
+    });
 
-	$(document).on( 'click', '.show-map a', function( event ) {
-		event.preventDefault();
-		showMap();
-	});
-	
+    $(document).on('click', '.show-map a', function (event) {
+        event.preventDefault();
+        showMap();
+    });
+
 })(jQuery);
 
 function hasScrolled() {
-	var st = $(this).scrollTop();
+    var st = $(this).scrollTop();
+    console.log(st);
 
-	// Make sure they scroll more than delta
-	if(Math.abs(lastScrollTop - st) <= delta) {
-		return;
-	}
+    // Make sure they scroll more than delta
+    if (Math.abs(lastScrollTop - st) <= delta) {
+        return;
+    }
 
-	// If they scrolled down and are past the navbar, add class .nav-up.
-	// This is necessary so you never see what is "behind" the navbar.
-	if (st > lastScrollTop && st > navbarHeight) {
-		hideMap();
-	} else {
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight) {
+        hideMap();
+    } else {
 
-		// Scroll Up
-		if(st <= 500) {
-			showMap();
-		}
-	}
+        // Scroll Up
+        if (st <= 500) {
+            showMap();
+        }
+    }
 
-	lastScrollTop = st;
+    lastScrollTop = st;
 }
